cc34cbfb2902aea93d7304d1a971df50d871263a
santin74 <santin74@gmail.com>
2015-07-29 11:29:24 +0200

    Lista preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,26 @@
 var models = require ('../models/models.js');
 
-// GET /quizes/question
+/* GET /quizes/question
 exports.question= function (req,res ) {
 	models.Quiz.findAll().success(function (quiz) {
     res.render('quizes/question', {pregunta : quiz[0].pregunta});
  })
 };
+*/
+
+exports.show = function (req, res ) {
+  models.Quiz.find(req.params.quizId).then(function(quiz) {
+  res.render('quizes/show', {quiz: quiz});
+  })
+};
+
 
  exports.answer = function (req,res) {
-	models.Quiz.findAll().success(function (quiz) {
-	  if (req.query.respuesta === quiz[0].respuesta){
-		 res.render ('quizes/answer', {respuesta: 'Correcto'});
+	models.Quiz.find(req.params.quizId).then(function (quiz) {
+	  if (req.query.respuesta === quiz.respuesta){
+		 res.render ('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
 		 } else {
-		 res.render ('quizes/answer', {respuesta: 'Incorrecto'});
+		 res.render ('quizes/answer', {quiz: quiz, respuesta: 'Incorrecto'});
 		 }
 	})
 };
@@ -20,3 +28,10 @@ exports.question= function (req,res ) {
 exports.author= function (req,res ) {
  res.render('author', {author : 'santin74'});
  };
+
+exports.index  =function (req,res ) {
+  models.Quiz.findAll().then(function(quizes){
+	  res.render('quizes/index.ejs', { quizes: quizes });
+	})
+ };
+
--- models/models.js
+++ models/models.js
@@ -32,12 +32,12 @@ var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz=Quiz;
 
-sequelize.sync().success(function () {
-
-  Quiz.count().success(function (count){
+sequelize.sync().then(function () {
+  Quiz.count().then(function (count){
 		if(count === 0 )  {
-			Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma' })
-		.success(function(){console.log('Base de datos inicializada')});
+			Quiz.create({ pregunta: 'Capital de Italia', respuesta: 'Roma' });
+		Quiz.create({ pregunta: 'Capital de Portugal', respuesta: 'Lisboa' })
+		.then(function(){console.log('Base de datos inicializada')});
 		};
 	});
 });
